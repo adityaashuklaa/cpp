f97f6256ff9ec62d7cd7929d0ff395e3b4bf206b
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-04-26 23:54:41 +0530

    Added vectors

--- stl.cpp
+++ stl.cpp
@@ -32,11 +32,47 @@ void vectors(){
     vector<int> v1(5, 20);
     vector<int> v2(v1);
 
-    vector<int>::iterator it = v.begin();
+    vector<int>::iterator it = v.begin(); // iterator points to the Memory Address.
 
     it++;
+    cout << *(it) << " "; // * gives the (element) at that address.
+
+    it = it + 2; 
     cout << *(it) << " ";
 
+    vector<int>::iterator it = v.end();
+    vector<int>::iterator it = v.rend(); // reverse-end
+    vector<int>::iterator it = v.rbegin();
 
+    cout << v[0] << " " << v.at(0);
+    cout << v.back() << " ";
     
+    for(vector<int>::iterator it = v.begin(); it != v.end(); it++){
+        cout << *(it) << " ";
+    }
+    for(auto it = v.begin(); it != v.end(); it++){ // Datatypes is automatically assigned acc. to the data.
+        cout << *(it) << " ";
+    }
+    for (auto it : v){
+        cout << it << " ";
+    }
+
+    v.erase(v.begin() + 1);
+    v.erase(v.begin() + 2, v.begin() + 4);
+
+    vector<int>v(2, 100);
+    v.insert(v.begin(), 300);
+    v.insert(v.begin() + 1, 2, 10);
+
+    vector<int> copy(2,50);
+    v.insert(v.begin(), copy.begin(), copy.end());
+
+    cout << v.size();
+
+    v.pop_back();
+
+    v1.swap(v2);
+    v.clear() // erases the entire vector
+
+    cout << v.empty();
 }
