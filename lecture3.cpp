c33f6f51451faa83bd4c0c0a1d205e9829c4d1f5
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-07-03 00:27:38 +0530

    Vectors in STL

--- lecture3.cpp
+++ lecture3.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 // DSA is all about learning Algorithms, Containers, Functions, Iterators.
 
-// Pairs
+// Pairs => It can be treated as a data type which lies inside utilities library.
 void explainPair(){
     pair<int, int> p = {1, 3};
     cout << p.first << " " << p.second;
@@ -16,5 +16,27 @@ void explainPair(){
 
     pair<int, int> arr[] = {{1,2}, {2,5}, {5,1}};
 
-    cout << arr[1].second
+    cout << arr[1].second // 5
+}
+
+// Vectors
+void explainVector(){
+
+    vector<int> v;
+    v.push_back(1);
+    v.emplace_back(2);
+
+    vector<pair<int, int>>vec;
+    v.push_back({1,2});
+    v.emplace_back({1,2});
+
+    vector<int> v(5, 100);
+
+    vector<int> v(5);
+    vector<int> v1(5, 20);
+    vector<int> v2(v1);
+
+    vector<int>::iterator it = v.begin();
+
+    cout << *(it) << " ";
 }
