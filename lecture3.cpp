d84cee9cbeaa7a24f0e9f69770167007a2d4e5e3
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-07-03 00:37:44 +0530

    Vectors in STL

--- lecture3.cpp
+++ lecture3.cpp
@@ -30,15 +30,32 @@ void explainVector(){
     v.push_back({1,2});
     v.emplace_back(1,2);  // Curly braces are not required, it by itself recognises it is a pair.
 
-    vector<int> v(5, 100);
+    vector<int> v(5, 100); // {100, 100, 100, 100, 100}
 
-    vector<int> v(5);
+    vector<int> v(5); 
     vector<int> v1(5, 20);
-    vector<int> v2(v1);
+    vector<int> v2(v1); // copying container to another vector.
 
     vector<int>::iterator it = v.begin();
-
+    it ++;
     cout << *(it) << " ";
+
+    it = it +2;
+    cout << *(it) << " ";
+
+    vector<int>::iterator it = v.end();
+    vector<int>::iterator it = v.rend();
+    vector<int>::iterator it = v.rbegin();
+
+    cout << v[0] << " " << v.at(0);
+    cout << v.back() << " ";
+
+    for (vector<int>::iterator it = v.begin(); it!= v.end(); it++){
+        cout << *(it) << " ";
+    }
+    for (auto it = v.begin(); it != v.end(); it++){
+        cout << *(it) << " ";
+    }
 }
 
 // Arrays sizes cannot be modified, that's why vectors comes into the picture, you can easily manipulate the size of the vector.
