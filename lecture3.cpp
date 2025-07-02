a2616fc118f729f8a6559eb0101c82490a4188f0
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-07-03 01:05:27 +0530

    Stack

--- lecture3.cpp
+++ lecture3.cpp
@@ -111,3 +111,25 @@ void explainDeque(){
     dq.front();
 
 }
+
+void explainStack(){
+    stack<int> st;
+    st.push(1); // {1}
+    st.push(2); // {2,1}
+    st.push(3); // {3,2,1}  
+    st.push(4); // {4,3,2,1}
+    st.emplace(5); // {5,4,3,2,1}}
+
+    cout << st.top(); // points 5
+
+    st.pop(); // {4,3,2,1}
+    cout << st.top(); // 4
+    cout << st.size(); // 4
+    cout << st.empty(); // false
+
+    stack<int>st1, st2;
+    st1.swap(st2);
+}
+// Stack follows LIFO, and all functions follow 0(1) time complexity.
+
+void explain
