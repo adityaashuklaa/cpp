532c58ccddb8d48936290cf36fae25c588cd3137
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-07-02 23:45:40 +0530

    Space Complexity

--- lecture2.cpp
+++ lecture2.cpp
@@ -1,4 +1,8 @@
 /* Time Complexity 
 Rate of which the time taken increases with respect to the input size.
+TC is computed in BigO Notation O()
+Always compute TC in the worst case scenario.
 
+Space Complexity is the combination of Auxilary space + Input space. 
+c = a+b; Here c is just storing the computed value, so it is auxilary.
 */
