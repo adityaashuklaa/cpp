23ba07573db5a590f6c3015ac9429bef5ea201d8
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-07-04 23:53:16 +0530

    Priority Queue

--- lecture3.cpp
+++ lecture3.cpp
@@ -143,3 +143,30 @@ void explainQueue(){
     cout << q.front(); // prints 1
     q.pop(); // {2,5}
 }
+
+void explainPQ(){
+    priority_queue<int>pq;  // The element which has the largest value stays at the top. It maintains tree ds inside.
+
+    pq.push(5); //{5}
+    pq.push(2); //{5, 2}
+    pq.push(8); //{8, 5, 2}
+    pq.emplace(10); //{10, 8, 5, 2}
+
+    cout << pq.top(); //{10}
+
+    pq.pop(); // {8, 5, 2}
+
+    pq.top(); // {8}
+    // size swap empty function same as others
+
+    // Minimun Heap (To store minimum value element at the top)
+    priority_queue<int, vector<int>, greater<int> pq;
+    pq.push(5); // {5}
+    pq.push(2); // {2, 5}
+    pq.push(8); // {2, 5, 8}
+    pq.emplace(10); // {2, 5, 8, 10}
+
+    cout << pq.top(); // prints 2
+
+    // Push and Pop happens in log n, and top happens in 0(1)
+}
