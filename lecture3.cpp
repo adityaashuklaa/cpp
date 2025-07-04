9d64df4ba0eb32c04a1039dfb27c900d68158b1f
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-07-05 00:29:19 +0530

    Introduced Map in STL

--- lecture3.cpp
+++ lecture3.cpp
@@ -231,4 +231,30 @@ void explainUset(){
     unordered_set<int> st;
 
     // lower_bound and upper_bound function does not work, rest all functions are same. It does not store in any particular order it has a better complexity than set in most cases, except some collison happens
+    // Time compleixity => 0(1) in rare case 0(N) linear time.
+}
+
+void explainMap(){
+    // Map is a container which stores everything in key and value pairs.
+    // Map stores unique keys in sorted order, something similar to set Data Structure.
+    map<int, int> mpp;
+    map<int, pair<int, int>> mpp;
+    map<pair<int, int>, int> mpp;
+
+    map[1] =2; // Key 1 store 2.
+    mpp.emplace({3, 1});
+    mpp.insert({2, 4});
+    mpp[{2,3}] = 10;
+    {
+        {1,2}
+        {2,4}
+        {3,1}
+    }
+
+    for(auto it: mpp) {
+        cout << it.first << " " << it.second << endl;
+    }
+
+    cout << mpp[1];
+    cout << mpp[5];
 }
