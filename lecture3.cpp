94d7af74a772a0cb450f24726b7904001ddbfb5a
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-07-05 00:14:13 +0530

    Unordered Set

--- lecture3.cpp
+++ lecture3.cpp
@@ -226,3 +226,9 @@ void explainMultiSet(){
 
     // rest all functions are same as set.
 }
+
+void explainUset(){
+    unordered_set<int> st;
+
+    // lower_bound and upper_bound function does not work, rest all functions are same. It does not store in any particular order it has a better complexity than set in most cases, except some collison happens
+}
