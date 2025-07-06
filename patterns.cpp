7c9595200e478dcfe0747f5870912ccdd9efe29f
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-07-06 22:49:34 +0530

    Pattern 6

--- patterns.cpp
+++ patterns.cpp
@@ -56,7 +56,7 @@ void print5(int n){
 void print6(int n){
     for(int i=1; i<n; i++){
         for(int j=1; j<n-i+1; j++){   
-            cout << j;
+            cout << j;   
         }
         cout << endl;
     }
