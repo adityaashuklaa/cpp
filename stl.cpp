2458e44d31531f117e3f11b316f0687f80e53051
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-07-08 22:37:53 +0530

    Vector Iterator

--- stl.cpp
+++ stl.cpp
@@ -5,7 +5,7 @@ void explainPair() {
 
     pair<int, pair<int, int>> p = {1, {3,4}};
 
-    cout << p.first << " " << p.second.first << " " << p.second.second;
+    cout << p.first << " " << p.second.first << " " << p.second.second;   
 
     pair<int, int> arr[] = { {1,2}, {2,3}, {3,4}};
 
