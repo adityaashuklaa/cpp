d48c6331a7b22ce4ebff2e4494018cf35dd9444c
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-07-12 23:19:40 +0530

    Time Complexity

--- lecture2.cpp
+++ lecture2.cpp
@@ -6,3 +6,4 @@ Always compute TC in the worst case scenario.
 Space Complexity is the combination of Auxilary space + Input space. 
 c = a+b; Here c is just storing the computed value, so it is auxilary.
 */
+  
