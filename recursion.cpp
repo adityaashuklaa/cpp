71047ad446ee3d2563d0b372a8d761d5ba661182
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-07-13 23:40:41 +0530

    String Palindrome

--- recursion.cpp
+++ recursion.cpp
@@ -79,7 +79,7 @@ void arrF(int i, int arr[], int n){
 bool f(int i, string &s){
     if(i >= s.size() / 2) return true;
     if(s[i] != s[s.size() - i - 1]) return false;
-    return f(i+1, s);
+    return f(i+1, s);   
 }
 
 int main(){
