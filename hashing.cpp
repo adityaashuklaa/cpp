79ba159c2879cf239d357ef5a724427d492ca948
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-07-16 00:41:56 +0530

    Starting with Character Hashing

--- hashing.cpp
+++ hashing.cpp
@@ -41,3 +41,23 @@ int main(){
 }
 
 // Character Hashing
+int main2(){
+    string s;
+    cin >> s;
+
+    // precompute
+    int hash[26] = {0};
+    for(int i = 0; i<s.size(); i++) {
+        hash[s[i] - 'a']++;
+    }
+
+    int q;
+    cin >> q;
+    while(q--) {
+        char c;
+        cin >> c;
+        // fetch
+        cout << hash[c - 'a'] << endl;
+    }
+    return 0;
+}
