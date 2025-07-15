3a6399d35979ca9a6c588acea89d452231383204
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-07-16 00:16:08 +0530

    Starting with Hashing

--- hashing.cpp
+++ hashing.cpp
@@ -1,1 +1,14 @@
 // Hashing
+// To find out the count of the number appearing in an array. 
+#include<iostream>
+using namespace std;
+
+int f(int number, int arr[]){
+    int cnt  = 0;
+    for(int i=0; i<number; i++){
+        if(arr[i] == number){
+            cnt = cnt + 1;
+        }
+    }
+    return cnt;
+} // This method is too time consuming and a heavy approach, there are better ways as well. 
