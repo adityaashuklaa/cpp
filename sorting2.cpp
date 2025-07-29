2ac3a2dd79e82b75449625310390478b45d58ad6
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-07-29 23:56:45 +0530

    Starting with merge sort

---
+++ sorting2.cpp
@@ -0,0 +1,7 @@
+// Merge Sort
+/*
+    There are three sorting algos but all of them where taking 0(n*2) TC, that's why Merge sort is used as it is more optimised in terms of TC.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
