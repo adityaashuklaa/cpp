f1334298e8c0e7a964e5f58d6477d04b2145a2c4
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-07-31 23:06:46 +0530

    Starting with merge sort

--- sorting2.cpp
+++ sorting2.cpp
@@ -2,6 +2,7 @@
 /*
     There are three sorting algos but all of them where taking 0(n*2) TC, that's why Merge sort is used as it is more optimised in terms of TC.
     Merge sort is based on Divide and Merge. 
+    Suppose you have an array of 9 elements, you have to divide them into two parts, and there are two ways to do that either 5/4 or 4/5. Here i'm following 5/4 path, and have to make sure i need to follow this approach throughout the sorting.
 */
 
 #include<bits/stdc++.h>
