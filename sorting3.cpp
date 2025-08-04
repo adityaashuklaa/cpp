64fcdb334eb125e2c92ecb37d3395ef142473947
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-08-04 22:41:03 +0530

    Starting with Quick Sort

--- sorting3.cpp
+++ sorting3.cpp
@@ -1,1 +1,5 @@
 // Quick Sort Algo
+ /*
+    Pick a pivot & place it in it's correct place in the sorted array.
+    Quick Sort doen't uses any extra temp array, it's slightly better than merge sort.
+ */
