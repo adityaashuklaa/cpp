b8ac55e8c168363d61de1448110bd87982c9c32e
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-08-04 23:12:01 +0530

    TC of Quick Sort Algo

--- sorting3.cpp
+++ sorting3.cpp
@@ -4,6 +4,7 @@
     Quick Sort doesn't uses any extra temp array, it's slightly better than merge sort. But it has the similar TC as merge sort.
     It's just basically pick up a element(pivot), place it to it's right position, then smaller on left and larger on right.
     It's a divide and conquere algo.
+    TC => nlogn
  */
 #include <bits/stdc++.h>
 using namespace std;
