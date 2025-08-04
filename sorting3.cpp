4b6c1a3992d0e27ecacefb04bba8325c7e115786
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-08-05 00:30:53 +0530

    TC of Quick Sort Algo

--- sorting3.cpp
+++ sorting3.cpp
@@ -12,7 +12,7 @@ int partition(vector<int> &arr, int low, int high){
     int pivot = arr[low];
     int i = low;
     int j = high;
-    while( i < j) {
+    while( i < j) {  
         while(arr[i] <= pivot && i <= high - 1) {
             i++;
         }
