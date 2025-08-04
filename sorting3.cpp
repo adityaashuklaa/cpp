e175f25f4e9d41fe47b9b2138034405f3574ca1a
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-08-04 23:11:32 +0530

    Quick Sort Algo Completed

--- sorting3.cpp
+++ sorting3.cpp
@@ -5,3 +5,33 @@
     It's just basically pick up a element(pivot), place it to it's right position, then smaller on left and larger on right.
     It's a divide and conquere algo.
  */
+#include <bits/stdc++.h>
+using namespace std;
+int partition(vector<int> &arr, int low, int high){
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+    while( i < j) {
+        while(arr[i] <= pivot && i <= high - 1) {
+            i++;
+        }
+        while(arr[j] > pivot && j >= low + 1) {
+            j--;
+        }
+        if(i<j) swap(arr[i], arr[j]);
+    }
+    swap(arr[low], arr[j]);
+    return j;
+}
+void qs(vector<int> &arr, int low, int high) {
+    if(low < high){
+        int pIndex = partition(arr, low, high);
+        qs(arr, low, pIndex - 1);
+        qs(arr, pIndex + 1, high);
+    }
+}
+
+vector<int> quickSort(vector<itn> arr){
+    qs(arr, 0, arr.size() - 1);
+    return arr;
+}
