2556d8054cf6d251de2d3129f27a994e0f75ded9
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-08-04 22:53:26 +0530

    Understanding Quick Sort Algo

--- sorting3.cpp
+++ sorting3.cpp
@@ -2,4 +2,5 @@
  /*
     Pick a pivot & place it in it's correct place in the sorted array.
     Quick Sort doesn't uses any extra temp array, it's slightly better than merge sort. But it has the similar TC as merge sort.
+    It's just basically pick up a element(pivot), place it to it's right position, then smaller on left and larger on right.
  */
