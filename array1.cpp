bacde0b09ee5ac3ce6c4f9df0a5930c424850fad
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-08-05 23:26:31 +0530

    Starting with Arrays

--- array1.cpp
+++ array1.cpp
@@ -1,5 +1,6 @@
 // Arrays
 /*
     Whenever array is defined inside the main then the array elements stores the garbage value and when the array is being defined in the global scope the elements carries value 0.
-    
+    Max size of the array when you define it inside main() it is 10pow6 and when it is being defined in the global scope it's size is 10pow7.
+    Array follows 0 based indexing, where first index is 0 itself, and last index is n-1.
 */
