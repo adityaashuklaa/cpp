79c4bfa05d2432584f682ce8591869d9b6dec5fa
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-08-05 23:23:49 +0530

    Starting with Arrays

--- array1.cpp
+++ array1.cpp
@@ -1,1 +1,5 @@
 // Arrays
+/*
+    Whenever array is defined inside the main then the array elements stores the garbage value and when the array is being defined in the global scope the elements carries value 0.
+    
+*/
