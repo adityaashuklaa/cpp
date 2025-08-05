fbbd66f1653d222f4d049ae051114081802868d3
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-08-06 01:04:44 +0530

    Second Largest element Optimal  approach.

--- array1.cpp
+++ array1.cpp
@@ -34,7 +34,7 @@ int largestElement(vector<int> &arr, int n) {
         }
         }
     } TC => 0(nlogn + n) if secondLargest doesn't exist then -1
-    if array contains a -ve number than INT_MIN should be used.
+    if array contains a -ve number than INT_MIN should be used instead of -1.
     Better Approach
     largest = arr[0];
     for(int i=0; i<n; i++){
@@ -52,7 +52,43 @@ int largestElement(vector<int> &arr, int n) {
     Tc => n+n = 0(2n)
 
     Optimal Approach
+    largest = arr[0], sLargest = -1
 */
+int secondLargest(vector<int> &a, int n){
+    int largest = a[0];
+    int slargest = -1;
+    for(int i=1; i<n; i++){
+        if(a[i] > largest){
+            slargest = largest;
+            largest = a[i];
+        }
+        else if(a[i] < largest && a[i] > slargest) {
+            slargest = a[i];
+        }
+    }
+    return slargest;
+}
+
+int secondLargest(vector<int> &a, int n){
+    int smallest = a[0];
+    int ssmallest = INT_MAX;
+    for(int i=1; i<n; i++){
+        if(a[i] < smallest){
+            ssmallest = smallest;
+            smallest = a[i];
+        }
+        else if(a[i] != smallest && a[i] < ssmallest) {
+            ssmallest = a[i];
+        }
+    }
+    return slargest;
+}
+
+vector<int> getSecondOrderElements(int n, vector<int> a){
+    int sLargest = secondLargest(a,n);
+    int sSmallest = secondSmallest(a,n);
+    return {sLargest, sSmallest}
+}
 
 // If given array is sorted or not.  TC  => Takes a single pass so takes 0(n)
 int isSorted(int n, vector<int> a){
