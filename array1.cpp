1bf0823bdc9101d81bfcf34140b5b9bdd3ed7c27
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-08-16 01:24:41 +0530

    Array largest integer

--- array1.cpp
+++ array1.cpp
@@ -15,7 +15,7 @@ int largestElement(vector<int> &arr, int n) {
     for(int i = 0; i<n; i++) {
         if(arr[i] > largest) {
             largest = arr[i];
-        }
+        }  
     }
     return largest;
 }
