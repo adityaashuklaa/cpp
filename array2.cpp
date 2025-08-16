2daf70b182ab2d1515972ae205703376f20b78b2
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-08-16 23:01:53 +0530

    Rotating Array by K places

--- array2.cpp
+++ array2.cpp
@@ -1,3 +1,12 @@
 // Left rotate the array by one place. 
 #include <bits/stdc++.h>
 using namespace std;    
+// Bruteforce Approach
+temp = arr[0];
+for(int i=1; i<n; i++){
+    arr[i-1] = arr[i];
+}
+arr[n-1] = temp;
+// TC => 0(n) SC => 0(1)
+
+// Left rotating array by D places.
