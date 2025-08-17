ce7c775dfcbd4ed1d13d6ee61f2f80ec33c3aace
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-08-18 01:38:11 +0530

    Rotating Array by K places

--- array2.cpp
+++ array2.cpp
@@ -10,3 +10,8 @@ arr[n-1] = temp;
 // TC => 0(n) SC => 0(1)
 
 // Left rotating array by D places.
+/*
+    arr[] = [1, 2, 3, 4, 5, 6, 7] 
+    If the rotation is being done for the 8 places it can be expressed as 7+1 rotations.
+    So, for no. of rotations D = d % n, whatever the remainder left, is the rotation.
+*/
