ea2b323b87f6b161fe89a144e5000e6b707bfc41
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-08-18 02:11:16 +0530

    Moving zeroes in the end Optimal Approach

--- array2.cpp
+++ array2.cpp
@@ -71,3 +71,23 @@ vector<int> moveZeroes(int n, vector<int> a){
 
     return a;
 }
+
+// Optimal Approach
+vector<int> moveZeroesOp(int n, vector<int> a){
+    int j = -1;
+    for(int i = 0; i<n; i++){
+        if(a[i] == 0){
+            j = i;
+            break;
+        }
+    }
+    if(j == -1) return a;
+
+    for(int i = j+1; i<n; i++){
+        if(a[i] != 0) {
+            swap(a[i], a[j]);
+            j++;
+        }
+    }
+    return a;
+}
