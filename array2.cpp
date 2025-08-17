3c8d990934c3ea575299f359259e12bcf87949e7
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-08-18 02:22:26 +0530

    Union of two sorted arrays bruteforce approach

--- array2.cpp
+++ array2.cpp
@@ -100,3 +100,21 @@ int linearSearch(int n, int num, vector<int>){
     }
     return -1;
 }
+
+// Union of two sorted arrays
+vector <int> sortedArray(vector <int> a, vector <int> b){
+    int n1 = a.size();
+    int n2 = b.size();
+    set<int> st;
+    for(int i = 0; i<n1; i++){
+        st.insert(a[i]);
+    }
+    for(int i = 0; i<n2; i++){
+        st.insert(b[i]);
+    }
+    vector<int> temp;
+    for(auto it : st){
+        temp.push_back(it);
+    }
+    return temp;
+}
