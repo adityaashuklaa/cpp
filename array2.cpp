b5fb1b9a95b3692fb061c46f1eac118dd97060dc
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-08-18 01:51:56 +0530

    Rotating Array by K places Algo

--- array2.cpp
+++ array2.cpp
@@ -14,4 +14,20 @@ arr[n-1] = temp;
     arr[] = [1, 2, 3, 4, 5, 6, 7] 
     If the rotation is being done for the 8 places it can be expressed as 7+1 rotations.
     So, for no. of rotations D = d % n, whatever the remainder left, is the rotation.
+    TC => 0(n+d);
+    SC => 0(d)
 */
+
+void leftRotate(int arr[], int n, int d){
+    d = d%n;
+    int temp[d];
+    for(int i=0; i<d; i++){
+        temp[i] = arr[i];
+    }
+    for(int i=d; i<n; i++){
+        arr[i-d] == arr[i];
+    }
+    for(int i = n-d; i<n; i++){
+        arr[i] = temp[i - (n-d)];
+    }
+}
