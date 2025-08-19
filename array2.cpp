83f9f057ff0d330abedbb4af825cd1d1c80f6b54
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-08-19 22:59:23 +0530

    Intersection of two sorted arrays.

--- array2.cpp
+++ array2.cpp
@@ -159,3 +159,24 @@ vector <int> sortedArrayOpt(vector <int> a, vector <int> b) {
     
     return unionArr;
 }
+
+// Intersection of two sorted arrays
+
+// Brute Force Approach for the Intersection 
+// TC => n1xn2
+vector<int> findArrayIntersection(vector<int> &A, int vector<int> &B, int m)
+{
+    vector<int> ans;
+    int vis[m] = {0};
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++) {
+            if(A[i] == B[j] && vis[j] == 0) {
+                ans.push_back(A[i]);
+                vis[j] = 1;
+                break;
+            }
+            if(B[j] > A[i]) break;
+        }
+    }
+    return ans;
+}
