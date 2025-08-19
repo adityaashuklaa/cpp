f44eef4c6f1c982a9d4c4c17f58537474c56d7be
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-08-19 22:44:28 +0530

    Optimal Approach for Union of Sorted arrays

--- array2.cpp
+++ array2.cpp
@@ -119,3 +119,43 @@ vector <int> sortedArray(vector <int> a, vector <int> b){
     }
     return temp;
 }
+// Optimal Approach
+// TC => 
+vector <int> sortedArrayOpt(vector <int> a, vector <int> b) {
+    int n1 = a.size();
+    int n2 = a.size();
+    int i = 0; 
+    int j = 0;
+    vector<int> unionArr;
+    while (i < n1 && j < n2)
+    {
+        if(a[i] <= b[j]){
+            if(unionArr.size() == 0 || unionArr.back() != a[i]) {
+                unionArr.push_back(a[i]);
+            }
+            i++;
+        }
+        else {
+            if(unionArr.size() == 0 || unionArr.back() != b[j]) {
+                unionArr.push_back(b[j]);
+            }
+            j++;
+        }
+    }
+    while (j < n2)
+    {
+        if(unionArr.size() == 0 || unionArr.back() != b[j]) {
+            unionArr.push_back(b[j]);
+        }
+        j++;
+    }
+    while (i < n1)
+    {
+        if(unionArr.size() == 0 || unionArr.back() != a[i]) {
+            unionArr.push_back(a[i]);
+        }
+        i++;
+    }
+    
+    return unionArr;
+}
