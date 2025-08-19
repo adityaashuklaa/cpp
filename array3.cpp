15f5ac883f865803696fa7de0d3c0e57e81d098f
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-08-20 00:46:54 +0530

    Bruteforce for Missing Number

--- array3.cpp
+++ array3.cpp
@@ -1,1 +1,12 @@
 // Finding Missing Number in Array.
+// Bruteforce Approach
+// for(int i=1; i<=n ; i++){ TC => in the worstcase the TC will be n*2 SC => 0(1)
+//     flag = 0
+//     for(int j=0; j<n-1; j++){
+//         if(arr[j] == i){
+//             flag = 1;
+//             break;
+//         }
+//     }
+//     if(flag == 0) return i;
+// }
