2a070e3c6aae134ad482a6d890dcbf63b3b4a186
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-08-20 00:50:24 +0530

    Better for Missing Number

--- array3.cpp
+++ array3.cpp
@@ -10,3 +10,17 @@
 //     }
 //     if(flag == 0) return i;
 // }
+
+// Better Approach (Using Hashing)
+/*
+    hash[n+1] = {0};
+    for(int i=0; i<n-1; i++){
+        hash[arr[i]] = 1;
+    }
+    for(int i=0; i<=n; i++){
+        if(hash[i] == 0){
+            return i;
+        }
+    }
+    TC => 0(n) + o(n) = 0(2n)
+*/
